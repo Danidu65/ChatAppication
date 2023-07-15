df3e1b17bec9adedd213720b848a904c8380df51
Danidu Viraj <daniduijse65@Gmail.com>
2023-07-15 22:06:05 +0530

    Commit

---
+++ src/main/java/Clinet/Clinet.java
@@ -0,0 +1,45 @@
+package Clinet;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.List;
+
+public class Clinet {
+    private Socket socket;
+    private List<Clinet> clinets;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+    private String msg = "";
+
+    public Clinet(Socket socket, List<Clinet> clinets) {
+    try {
+        this.socket = socket;
+        this.clinets = clinets;
+        this.dataInputStream = new DataInputStream(socket.getInputStream());
+        this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                    try {
+                        while (socket.isConnected()) {
+                            msg = dataInputStream.readUTF();
+                            for (Clinet client : clinets) {
+                                if (client.socket.getPort() != socket.getPort()) {
+                                    client.dataOutputStream.writeUTF(msg);
+                                    client.dataOutputStream.flush();
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                
+            }
+        });
+    }
+}
