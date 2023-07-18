b36f6c8dd49613873e2a9a93d81bcd6fc777137f
Danidu Viraj <daniduijse65@Gmail.com>
2023-07-18 22:14:17 +0530

    Commit

--- src/main/java/Server/server.java
+++ src/main/java/Server/server.java
@@ -1,13 +1,10 @@
 package Server;
-
 import Clinet.Clinet;
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-
 public class Server {
     private ServerSocket serverSocket;
     private Socket socket;
